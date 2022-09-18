f75b99b9e446e5a788a1042a3734f5907fba2d80
Le Vivilet <levivlet@pm.me>
2022-09-18 08:36:49 +0200

    update icons to v11.17.0

--- scripts/update-icons.js
+++ scripts/update-icons.js
@@ -9,7 +9,7 @@ import { pipeline } from 'stream/promises'
 import { fileURLToPath } from 'url'
 import VError from 'verror'
 
-const VERSION = '11.16.0'
+const VERSION = '11.17.0'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const outFileZip = resolve(__dirname, `../.tmp/extension-${VERSION}.zip`)
