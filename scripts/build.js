749ab3200653227974c87b411d0fbd757f0668ba
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-10-07 17:06:12 +0200

    simplify build script (#11)

--- scripts/build.js
+++ scripts/build.js
@@ -1,31 +1,25 @@
-import fs from 'node:fs';
-import path, {dirname, join} from 'node:path';
-import {fileURLToPath} from 'node:url';
-import {packageExtension} from '@lvce-editor/package-extension';
+import { copyFiles, packageExtension } from '@lvce-editor/package-extension'
+import path, { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const root = path.join(__dirname, '..');
+const root = path.join(__dirname, '..')
 
-fs.rmSync(join(root, 'dist'), {recursive: true, force: true});
-
-fs.mkdirSync(path.join(root, 'dist'));
-
-fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'));
-fs.copyFileSync(
-	join(root, 'extension.json'),
-	join(root, 'dist', 'extension.json'),
-);
-fs.copyFileSync(
-	join(root, 'icon-theme.json'),
-	join(root, 'dist', 'icon-theme.json'),
-);
-fs.cpSync(join(root, 'icons'), join(root, 'dist', 'icons'), {
-	recursive: true,
-});
+await copyFiles({
+  root,
+  files: [
+    'README.md',
+    'extension.json',
+    'icon-theme.json',
+    'icons',
+    'icon.png',
+  ],
+  outDir: join(root, 'dist'),
+})
 
 await packageExtension({
-	highestCompression: true,
-	inDir: join(root, 'dist'),
-	outFile: join(root, 'extension.tar.br'),
-});
+  highestCompression: true,
+  inDir: join(root, 'dist'),
+  outFile: join(root, 'extension.tar.br'),
+})
